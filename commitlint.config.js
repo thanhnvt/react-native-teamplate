006fb7251ec2f3cc7908cf2bd660a564a9811798
Thanh Nguyen <nguyenthanhhdlhld@gmail.com>
2023-07-13 14:47:07 +0700

    feat: Add new feature

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    'subject-case': [
+      'sentence-case',
+      'start-case',
+      'pascal-case',
+      'upper-case',
+    ],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'custom'],
+    ],
+  },
+};
